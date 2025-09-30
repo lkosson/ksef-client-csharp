6b83111328f28979ac5f2efc0166d33f12d19e2a
Łukasz Kosson <git@lukasz.kosson.net>
2025-09-30 13:39:19 +0200

    Projekt-piaskownica do eksperymentów

---
+++ Scratchpad/Program.cs
@@ -0,0 +1,10 @@
+﻿using KSeF.Client;
+using KSeF.Client.DI;
+using Microsoft.Extensions.DependencyInjection;
+
+var sc = new ServiceCollection();
+sc.AddKSeFClient(opts => { opts.BaseUrl = KsefEnviromentsUris.TEST; opts.CustomHeaders = []; });
+using var sp = sc.BuildServiceProvider();
+var ksefClient = sp.GetRequiredService<IKSeFClient>();
+
+Console.WriteLine("Ok");
